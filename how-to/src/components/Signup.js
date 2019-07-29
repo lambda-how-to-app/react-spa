3ed1866cfcd7445d82ad9629880729f9c88f5331
Lou <antilou86@gmail.com>
2019-07-29 16:03:00 -0400

    WIP-SignUp Component- added form fields and data handling for all fields of the form. for correctly pulls inputted fields and can handle button click. need to add styling in-line with UX guide and connect with lead React dev to ensure data reaches the correct endpoints. moving on to styling first.

--- how-to/src/components/Signup.js
+++ how-to/src/components/Signup.js
@@ -1,11 +1,69 @@
-import React from "react";
+import React, {useState} from "react";
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
 
 const Signup = () => {
+
+  const [inputValue, setInputValue] = useState({firstname: '', lastname: '', email:'', psw:'', confirmpsw:''});
+
+  const changeHandler = event => {
+    setInputValue({...inputValue, [event.target.name]:event.target.value});
+  };
+  
+  const passwordChecker = (p1, p2) => {
+    if (p1 !== p2) {
+      alert('passwords do not match, please try again');
+    } else {
+      return;
+    }}
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    //This needs to be changed to a function that matched email/token and updates data on the backend.
+    console.log(inputValue);  
+    setInputValue({firstname: '', lastname: '', email:'', psw:'', confirmpsw:''});
+  }
+
+  
+
   return (
+    <>
     <div>
-      <h2>Signup Component</h2>
+      <h1> Welcome To How-To </h1>
     </div>
+    <fieldset> {/*background container for login fields*/}
+      <form onSubmit={event=>handleSubmit(event)}>
+          <label>
+            First Name
+            <input type="text"  name="firstname" value={inputValue.firstname}onChange={changeHandler}/>
+          </label>
+          <label>
+            Last Name
+            <input type="text"  name="lastname"value={inputValue.lastname}onChange={changeHandler}/>
+          </label>
+
+          <label>
+            Email Address
+            <input type="email" name="email" value={inputValue.email}onChange={changeHandler}/>
+          </label>
+          <label>
+            Password
+            <input type="password" name="psw" value={inputValue.psw}onChange={changeHandler}/>
+          </label>
+          <label>
+            Confirm Password
+            <input type="password" name="confirmpsw" value={inputValue.confirmspw}onChange={changeHandler}/>
+          </label>
+      
+          <button>Sign Up</button>
+          <p>Already have an account? <Link to="/Login">Sign In</Link> here!</p>
+      </form>
+    </fieldset>
+    </>
   );
 };
 
 export default Signup;
+
+
+//LOU THIS IS WHAT YOU'RE WORKING ON
