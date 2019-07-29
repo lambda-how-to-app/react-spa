7b86cd11bb952a00d45483a676090a05df8de92a
bharti3bk <bharti3bk@gmail.com>
2019-07-29 13:08:48 -0700

    Updated Welcome.js

--- how-to/src/components/Welcome.js
+++ how-to/src/components/Welcome.js
@@ -3,30 +3,27 @@ import React from 'react';
 
 function Welcome() {
     return (
-        <div>
-            <form class="ui form">
-                <div class="field">
-                    <label>First Name</label>
-                    <input type="text" name="first-name" placeholder="First Name" />
-                </div>
-                <div class="field">
-                    <label>Last Name</label>
-                    <input type="text" name="last-name" placeholder="Last Name" />
-                </div>
-                <div class="field">
-                    <label>Email Address</label>
-                    <input type="text" name="email-address" placeholder="Email Address" />
-                </div> 
-                <div class="field">
-                    <label>Password</label>
-                    <input type="text" name="password" placeholder="Password" />
-                </div>  
-                <div class="field">
-                    <label>Confirm Password</label>
-                    <input type="text" name="confirm password" placeholder="Confirm Password" />
-                </div> 
-                <button class="ui button" type="submit">Submit</button>
-            </form>
+        <div> 
+            <div className ="field">
+            <button class="ui button"> Creator Account </button>  
+            <div class="content">
+           <div class="header"><i class="right triangle icon"></i> Organize how-to tutorials</div>
+            <div class="header">  <i class="right triangle icon"></i> Use Photos and videos</div>
+            <div class="header">  <i class="right triangle icon"></i> Gain followers</div>
+            <div class="header">  <i class="right triangle icon"></i> Share guides privately OR publicly</div>
+           </div>
+           
+           <button class="ui button"> General Account </button>  
+            <div class="content">
+           <div class="header"><i class="right triangle icon"></i> find tutorials and other guides</div>
+            <div class="header">  <i class="right triangle icon"></i>easily search/filter how-to guides</div>
+            <div class="header">  <i class="right triangle icon"></i> follow your favorite creators</div>
+            <div class="header">  <i class="right triangle icon"></i> share guides with friends</div>
+           </div>  
+
+           <button class="ui button"> Sign Up </button>
+
+        </div> 
         </div>
     );
 }
