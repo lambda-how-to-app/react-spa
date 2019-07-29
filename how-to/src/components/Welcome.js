809f13e5af8da2cb156462dffbc50da368ded10f
bharti3bk <bharti3bk@gmail.com>
2019-07-29 14:26:49 -0700

    Modified Welcome.js

--- how-to/src/components/Welcome.js
+++ how-to/src/components/Welcome.js
@@ -1,31 +1,55 @@
 import React from 'react';
+import '../../src/index.css'
 
 
 function Welcome() {
     return (
-        <div> 
-            <div className ="field">
-            <button class="ui button"> Creator Account </button>  
-            <div class="content">
-           <div class="header"><i class="right triangle icon"></i> Organize how-to tutorials</div>
-            <div class="header">  <i class="right triangle icon"></i> Use Photos and videos</div>
-            <div class="header">  <i class="right triangle icon"></i> Gain followers</div>
-            <div class="header">  <i class="right triangle icon"></i> Share guides privately OR publicly</div>
-           </div>
-           
-           <button class="ui button"> General Account </button>  
-            <div class="content">
-           <div class="header"><i class="right triangle icon"></i> find tutorials and other guides</div>
-            <div class="header">  <i class="right triangle icon"></i>easily search/filter how-to guides</div>
-            <div class="header">  <i class="right triangle icon"></i> follow your favorite creators</div>
-            <div class="header">  <i class="right triangle icon"></i> share guides with friends</div>
-           </div>  
+        <div>
+            <h1 class="ui header">WELCOME TO HOW-TO</h1>
+            <div className="field">
+                <button class="ui button" id="CreatorButton">
+                    <div class="ui right floated compact segment small">
+                        <div class="ui fitted checkbox">
+                            <input type="checkbox" />
+                            <label></label>
+                        </div>
+                    </div>
+                    <div id="CreatorAcc"> Creator Account </div>
+                    <br />
+                    <div class="content" id="FirstContent">
+                        <div class="header"><i class="right triangle icon"></i> Organize how-to tutorials</div>
+                        <div class="header">  <i class="right triangle icon"></i> Use Photos and videos</div>
+                        <div class="header">  <i class="right triangle icon"></i> Gain followers</div>
+                        <div class="header">  <i class="right triangle icon"></i> Share guides privately OR publicly</div>
+                    </div>
+                </button>
+            </div><br />
 
-           <button class="ui button"> Sign Up </button>
-
-        </div> 
+            <div className="field" >
+                <button class="ui button" id="GenralButton">
+                    <div class="ui right floated compact segment ">
+                        <div class="ui fitted checkbox">
+                            <input type="checkbox" />
+                            <label></label>
+                        </div>
+                    </div>
+                    <div id="GeneralAcc"> General Account </div>
+                    <br />
+                    <div class="content" id="secondContent">
+                        <div class="header"><i class="right triangle icon"></i> find tutorials and other guides</div>
+                        <div class="header">  <i class="right triangle icon"></i>easily search/filter how-to guides</div>
+                        <div class="header">  <i class="right triangle icon"></i> follow your favorite creators</div>
+                        <div class="header">  <i class="right triangle icon"></i> share guides with friends</div>
+                    </div>
+                </button>
+            </div><br />
+            <div className="field">
+                <button class="ui button" id="signUpButton"> Sign Up </button>
+            </div>
         </div>
     );
 }
 
 export default Welcome;
+
+
