d4dd0677cc21967607456cb71204c6b31154987e
Vicky <vyue013@gmail.com>
2019-07-29 15:39:36 -0700

    updated styling, added handling for all fields

--- how-to/src/components/Login.js
+++ how-to/src/components/Login.js
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form, Header } from 'semantic-ui-react';
+import './Login.css';
 
 const Login = () => {
+
+    // const { submitLogin } = props;
+    const [user, setUser] = useState({ email: "", password: "" });
+
+    function handleChange(event) {
+        const loginUser = { ...user, [event.target.name]: event.target.value };
+        setUser(loginUser);
+        console.log("handleChange", loginUser);
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        // submitLogin(user);
+        console.log("submitted: ", user);
+        setUser({ email: "", password: "" });
+    };
+
     return (
-        <div>
+        <div className="ui center aligned container">
             <Header as='h1'>WELCOME BACK</Header>
-            <Form>
-                <Form.Field>
-                    <label>Email Address</label>
-                    <input placeholder='Email Address' />
+            <Form onSubmit={handleSubmit} className="formContainer">
+                <Form.Field className="emailContainer">
+                    <label htmlFor="email" className="emailLabel">Email Address</label>
+                    <input 
+                        placeholder='Email Address' 
+                        value={user.email}
+                        name="email"
+                        type="email"
+                        onChange={handleChange}
+                    />
                 </Form.Field>
                 <Form.Field>
-                <label>Password</label>
-                <input type='password' placeholder='Password' />
+                    <label htmlFor="password" className="pwLabel">Password</label>
+                    <input 
+                        placeholder='Password' 
+                        value={user.password}
+                        name="password"
+                        type="password"
+                        onChange={handleChange}
+                    />
                 </Form.Field>
-                <p>
-                    <a href="#">Forgot Your Password?</a>
+                <p className="resetCred">
+                    <a href="#" className="forgotPw">Forgot Your Password?</a>
                 </p>
-                <Button type='submit'>Sign In</Button>
-                <p>
-                    Don't have an account? <a href="#">Sign Up</a> here!
+                <Button type='submit' className="loginButton">Sign In</Button>
+                <p className="resetCred">
+                    Don't have an account? <a href="#" className="newAcct">Sign Up</a> here!
                 </p>
             </Form>
         </div>
@@ -27,3 +57,39 @@ const Login = () => {
 }
 
 export default Login;
+
+// need onClick for Sign Up & Forgot Password
+
+
+            {/* 
+            <h1 class="ui header">WELCOME BACK</h1>
+            <form class="ui form">
+                <div class="field">
+                    <label htmlFor="email">Email Address</label>
+                    <input
+                        placeholder='Email Address'
+                        value={user.email}
+                        name="email"
+                        type="email"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div class="field">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        placeholder='Password'
+                        value={user.password}
+                        name="password"
+                        type="password"
+                        onChange={handleChange}
+                    />
+                </div>
+                <p className="resetCred">
+                    <a href="#" className="forgotPw">Forgot Your Password?</a>
+                </p>
+                <button type="submit" className="button signInButton">Sign In</button>
+                <p className="resetCred">
+                    Don't have an account? <a href="#" className="newAcct">Sign Up</a> here!
+                </p>
+            </form>
+            */}
