c26a7218b76f6846a56718573913d61f9735fc18
Lou <antilou86@gmail.com>
2019-07-30 13:54:15 -0400

    WIP: some basic styling added. need to add a little more before i move on to another component

--- how-to/src/components/CreatorDashboard.js
+++ how-to/src/components/CreatorDashboard.js
@@ -1,52 +1,87 @@
 import React from "react";
+import {Link} from 'react-router-dom';
+import styled from 'styled-components'
 
+const Span = styled.span`
+width: 344px;
+height: 219px;
+background: #B89498;
+
+`
+
+const ProfileImage = styled.img `
+  width: 116px;
+  height: 116px;
+  border-radius: 50%;
+`
+
+const H3 = styled.h3 `
+  width: 198px;
+  height: 44px;
+  font-family: Nunito;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 44px;
+  text-align: center;
+  color: #000000;
+  `
+const LineDiv = styled.div `
+  width: 362px;
+  height: 8px;
+  top: 9px;
+  background: #5C5C5C;
+`
 const CreatorDashboard = () => {
+
+
   return (
     <>
-      <span>{/*user card */}
-        <img src={/*pulled in from the back end*/} alt="your uploaded user-photo" />
-        <h4>Creator_name</h4>
-        <link to="/settings"><p>edit</p></link>
-      </span>
-      <div>{/*grey line under the user card */}</div>
+      <Span>{/*user card */}
+        <ProfileImage src={/*pulled in from the back end*/} alt="your uploaded user-photo" />
+        <H3>Creator_name{/*actual name will be pulled in from backend*/}</H3>
+        <Link to="/settings"><img src="../../public/images/edit-icon.png" alt="edit icon"/></Link>
+      </Span>
+
+      <LineDiv>{/*grey line under the user card */}</LineDiv>
      
       <div> {/* guide-links card container */}
         
-        <link to="/Guides">
+        <Link to="/Guides">
           <div>{/*box link thing*/}
             <p>Browse Guides</p>
           </div>
-        </link>
+        </Link>
 
-        <link to="/Myguides">
+        <Link to="/Myguides">
           <div>{/*box link thing*/}
             <p>My Guides</p>
           </div>
-        </link>
+        </Link>
 
-        <link to="/AddGuide">
+        <Link to="/AddGuide">
           <div>{/*box link thing*/}
             <p>Add Guide</p>
           </div>
-        </link>
+        </Link>
 
-        <link to="/EditGuide">
+        <Link to="/EditGuide">
           <div>{/*box link thing*/}
             <p>Edit Guide</p>
           </div>
-        </link>
+        </Link>
 
-        <link to="/Followers">
+        <Link to="/Followers">
           <div>{/*box link thing*/}
             <p>Followers</p>
           </div>
-        </link>
+        </Link>
 
-        <link to="/Settings">
+        <Link to="/Settings">
           <div>{/*box link thing*/}
             <p>Settings</p>
           </div>
-        </link>
+        </Link>
       </div>
     </>
   )
