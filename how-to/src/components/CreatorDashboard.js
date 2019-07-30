91e4d6f9a3bdb36c4c716d1f0adf7bafa04a022c
Lou <antilou86@gmail.com>
2019-07-30 14:03:55 -0400

    WIP: basic styling completed. could use some flex-direction. moving on in the interest of time.

--- how-to/src/components/CreatorDashboard.js
+++ how-to/src/components/CreatorDashboard.js
@@ -2,12 +2,17 @@ import React from "react";
 import {Link} from 'react-router-dom';
 import styled from 'styled-components'
 
-const Span = styled.span`
-width: 344px;
-height: 219px;
-background: #B89498;
-
+const BodyWrap = styled.div `
+  background-color: #E5E5E5;
+  height: auto;
 `
+
+const Span = styled.span`
+  width: 344px;
+  height: 219px;
+  background: #B89498;
+  top: 61px;
+  `
 
 const ProfileImage = styled.img `
   width: 116px;
@@ -32,11 +37,35 @@ const LineDiv = styled.div `
   top: 9px;
   background: #5C5C5C;
 `
+
+const CardContainer = styled.div `
+  width:372px;
+  margin: 0 auto;
+`
+
+const Card = styled.div `
+  width: 132px;
+  height: 132px;
+  background: #678650;
+`
+
+const CardP = styled.p `
+  width: 83px;
+  height: 50px;
+  font-family: Nunito;
+  font-style: normal;
+  font-weight: 800;
+  font-size: 18px;
+  line-height: 25px;
+  text-align: center;
+  letter-spacing: 21px;
+  color: #FFFFFF;
+`
 const CreatorDashboard = () => {
 
 
   return (
-    <>
+    <BodyWrap>
       <Span>{/*user card */}
         <ProfileImage src={/*pulled in from the back end*/} alt="your uploaded user-photo" />
         <H3>Creator_name{/*actual name will be pulled in from backend*/}</H3>
@@ -45,45 +74,45 @@ const CreatorDashboard = () => {
 
       <LineDiv>{/*grey line under the user card */}</LineDiv>
      
-      <div> {/* guide-links card container */}
+      <CardContainer> {/* guide-links card container */}
         
         <Link to="/Guides">
-          <div>{/*box link thing*/}
-            <p>Browse Guides</p>
-          </div>
+          <Card>{/*box link thing*/}
+            <CardP>Browse Guides</CardP>
+          </Card>
         </Link>
 
         <Link to="/Myguides">
-          <div>{/*box link thing*/}
-            <p>My Guides</p>
-          </div>
+          <Card>{/*box link thing*/}
+            <CardP>My Guides</pCardP>
+          </Card>
         </Link>
 
         <Link to="/AddGuide">
-          <div>{/*box link thing*/}
-            <p>Add Guide</p>
-          </div>
+          <Card>{/*box link thing*/}
+            <CardP>Add Guide</CardP>
+          </Card>
         </Link>
 
         <Link to="/EditGuide">
-          <div>{/*box link thing*/}
-            <p>Edit Guide</p>
-          </div>
+          <Card>{/*box link thing*/}
+            <CardP>Edit Guide</CardP>
+          </Card>
         </Link>
 
         <Link to="/Followers">
-          <div>{/*box link thing*/}
-            <p>Followers</p>
-          </div>
+          <Card>{/*box link thing*/}
+            <CardP>Followers</CardP>
+          </Card>
         </Link>
 
         <Link to="/Settings">
-          <div>{/*box link thing*/}
-            <p>Settings</p>
-          </div>
+          <Card>{/*box link thing*/}
+            <CardP>Settings</CardP>
+          </Card>
         </Link>
-      </div>
-    </>
+      </CardContainer>
+    </BodyWrap>
   )
 
 
