68a8847bcd293df3cd34629759c7d87fe62a859e
Lou <antilou86@gmail.com>
2019-07-30 15:14:55 -0400

    WIP: initial component creation

---
+++ how-to/src/components/AddGuide.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+import styled from 'styled-components'
+
+const AddGuide = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default AddGuide;
