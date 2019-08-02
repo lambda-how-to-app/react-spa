6921faa8fb7989869faf2692b7ffccf0672bf510
Mike VanSleen <mike.vansleen@gmail.com>
2019-08-02 02:58:57 -0600

    Styled welcome comp, fixed navbar more

--- how-to/src/components/Welcome.js
+++ how-to/src/components/Welcome.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../src/index.css";
 import { Link } from "react-router-dom";
-
+import activity from "../../src/activity.JPG";
 // testing use: remove imports later
 import Guide from "./Guide.js";
 import MyGuides from "./MyGuides.js";
@@ -10,6 +10,11 @@ function Welcome() {
   return (
     <div>
       <h1 className="ui header">WELCOME TO HOW-TO</h1>
+      <img src={activity} />
+      <h5>
+        Make how-to guides, save them for later. Learn how to do new things.
+        Most importantly, have fun.
+      </h5>
     </div>
   );
 }
