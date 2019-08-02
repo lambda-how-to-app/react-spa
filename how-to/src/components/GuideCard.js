f8de53b823e55003762e0af3e611336eadd7aea4
Lou <antilou86@gmail.com>
2019-08-02 07:22:21 -0400

    Styled GuideList, GuideCard and MyGuides

--- how-to/src/components/GuideCard.js
+++ how-to/src/components/GuideCard.js
@@ -1,18 +1,74 @@
 import React from "react";
-import { Card, Image } from "semantic-ui-react";
-
+import styled from "styled-components"
 const GuideCard = ({ guide, users }) => {
   const guideUserId = guide.guide_auth_id;
+
+  const StyledCard = styled.div`
+    width: 300px;
+    height: 200px;
+    margin:10px auto;
+    background-color: #7EA85E; 
+    display: flex;
+    flex-flow: row nowrap;
+   `
+  const TextContent = styled.div`
+    width: 60%
+    display: flex;
+    flex-flow: column nowrap;
+  `
+   const StyledSpan = styled.span `
+   width: 90px;
+   height: 90px;
+   margin-left: 10px;
+   margin-top: 5px;
+   `
+
+  const CircleImage = styled.img`
+    width: 90px;
+    height: 90px;
+    border-radius: 50%;
+
+`
+  const AychFour = styled.h4 `
+  font-family: Nunito;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 20px;
+  float: right;
+  
+  `
+  const AychFourWrap = styled.div `
+    width:90%;
+    margin-top: 10px;
+    margin-right:10px;
+  `
+  const PWrap = styled.div `
+    width:100%;
+    margin-top: 50px;
+    height: 100px;
+  `
+  const Para = styled.p `
+    width:100%;
+    text-align: center;
+    overflow-wrap: break-word;
+  `
   return (
 
-    <Card>
-      <Card.Content>
-        <Image src={guide.banner_image} size='small' circular />
-        <Card.Header>{guide.title}</Card.Header>
-        <Card.Meta>{guide.created_at}</Card.Meta>
-        <Card.Description>Guide description goes here</Card.Description>
-      </Card.Content>
-    </Card>
+    <StyledCard>
+      <StyledSpan>
+        <CircleImage src={guide.banner_image ? guide.banner_image : null} />
+      </StyledSpan>
+      {/* <Card.Meta>{guide.created_at}</Card.Meta> */}
+     
+      <TextContent>
+        <AychFourWrap>
+          <AychFour>{guide.title}</AychFour>
+       </AychFourWrap>
+        <PWrap>
+          <Para>Guide description goes here. drink some water. hydrating is important for all kinds of sandpaper eyeballs.</Para>
+        </PWrap>
+      </TextContent>
+    </StyledCard>
   );
 };
 
--- how-to/src/components/MyGuides.js
+++ how-to/src/components/MyGuides.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Header, Card, Image, Pagination } from "semantic-ui-react";
+import { Header, Pagination } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { connect } from "react-redux";
@@ -11,20 +11,32 @@ import choco from "../chocomilk.jpg";
 import "./MyGuides.css";
 
 const GuideContainer = styled.div`
+  width: 95%;
   margin: 0 auto;
   display: flex;
-  align-items: center;
-  flex-direction: column;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
 `;
 
 const LineDiv = styled.div`
-  width: 353px;
+  width: 95%;
+  margin: 0 auto;
   height: 8px;
-  top: 9px;
   background: #5c5c5c;
   margin-bottom: 14px;
 `;
 
+const AychThree = styled.h3`
+  width: 100%;
+  text-align:center;
+`
+
+const StyledLink = styled(Link)`
+  &&&{
+    text-decoration: none !important;
+    color: black;
+  }
+`
 // to edit with user's guide id
 const handleClick = () => {
   console.log("guide clicked!");
@@ -47,7 +59,6 @@ function MyGuides({ guides, users, getGuides, getUsers }) {
         <LineDiv>{/*grey line under the user card */}</LineDiv>
 
         {/* input sort/filter functionality here */}
-        <p className-="sortTag">Sort/Filter</p>
         {guides && userId
           ? guides
               .sort((a, b) => {
@@ -55,9 +66,9 @@ function MyGuides({ guides, users, getGuides, getUsers }) {
               })
               .filter(guide => guide.guide_auth_id === userId)
               .map((guide, index) => (
-                <Link key={index} to={`/guide/${guide.id}`}>
+                <StyledLink key={index} to={`/guide/${guide.id}`}>
                   <GuideCard key={index} users={users} guide={guide} />
-                </Link>
+                </StyledLink>
               ))
           : null}
 
